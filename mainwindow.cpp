1ca774c0e89b1048d4e0ca6fab17e7e7d5c0177f
hoshianaaa <hoshina12345@gmail.com>
2021-12-12 15:18:56 +0900

    set button to table

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,7 +11,17 @@ MainWindow::MainWindow(QWidget *parent)
 {
 
     ui->setupUi(this);
-    ui->label->setText("Hello World!");
+
+    ui->tableWidget->setCellWidget(0,4,ui->pushButton_1);
+    ui->tableWidget->setCellWidget(1,4,ui->pushButton_2);
+    ui->tableWidget->setCellWidget(2,4,ui->pushButton_3);
+    ui->tableWidget->setCellWidget(3,4,ui->pushButton_4);
+    ui->tableWidget->setCellWidget(4,4,ui->pushButton_5);
+    ui->tableWidget->setCellWidget(5,4,ui->pushButton_6);
+    ui->tableWidget->setCellWidget(6,4,ui->pushButton_7);
+    ui->tableWidget->setCellWidget(7,4,ui->pushButton_8);
+    ui->tableWidget->setCellWidget(8,4,ui->pushButton_9);
+
     points_sub_ = nh_.subscribe("checker_detector/detect_points", 10, &MainWindow::pointsCallback, this);
     printf("register\n");
 }
